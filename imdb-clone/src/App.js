a42cbe1d76d8e94c1a37bd0b5505d993b672eca6
M. Abdullah Butt <81871162+AbdullahButt2611@users.noreply.github.com>
2023-02-12 23:38:58 +0500

    Working in Popular Category

--- imdb-clone/src/App.js
+++ imdb-clone/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header.js';
+import Header from './components/header/Header.js';
+import Home from './pages/home/home.js';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route index element={<h1>Hello World 2</h1>}></Route>
+          <Route index element={< Home />}></Route>
           <Route path='movie/:id' element={<h1>Movie Detail Page</h1>}></Route>
           <Route path='movies/:type' element={<h1>Movies List Page</h1>}></Route>
           <Route path='/*' element={<h1>Error Page</h1>}></Route>
